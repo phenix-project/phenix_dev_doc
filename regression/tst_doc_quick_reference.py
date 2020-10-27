4b9ace4afcf4f7f3fa257206e1a51b6eade6426a
dcliebschner <dcliebschner@lbl.gov>
2020-10-26 18:42:11 -0700

    test

--- regression/tst_doc_quick_reference.py
+++ regression/tst_doc_quick_reference.py
@@ -4,8 +4,9 @@ import libtbx.load_env
 from phenix_dev_doc.regression.exercise import exercise
 
 def run():
-  script_fn = "doc_quick_reference.py"
-  exercise(script = script_fn, tmp_path = 'tmp_files_8')
+  return_code = exercise(script   = 'doc_quick_reference.py',
+                         tmp_path = 'tmp_files_8')
+  return return_code
 
 if __name__ == '__main__':
   sys.exit(run())
